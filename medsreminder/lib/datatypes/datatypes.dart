5e08bc870ec89d9182ad26d5db8dbde3e7d667bd
Josef Miko Urquico <73434242+Special70@users.noreply.github.com>
2025-10-10 14:39:37 +0800

    upload of files

---
+++ medsreminder/lib/datatypes/datatypes.dart
@@ -0,0 +1,19 @@
+
+enum TimeType {
+  TIME_SCHEDULE, // rings at a specific hour of the day
+  REPEAT_TIME_SCHEDULE, // rings every set amount of day
+  ONCE_A_DAY, // no ringing but for letting the user know they consumed their medicine for the day
+}
+
+class Reminder {
+  TimeType? time_type;
+  bool? isEnabled;
+  String? label;
+
+  DateTime? time; // for TIME_SCHEDULE
+  List<DateTime>? times; // for REPEAT_TIME_SCHEDULE
+  bool? isTaken; // only for ONCE_A_DAY
+
+  DateTime? lastTaken; // saves the last time the user consumed their medicine.
+
+}
